bc83de283c59db371ede642ef1cd9e79c797a0ba
OndraNeveril <ondra.neveril@centrum.cz>
2025-05-22 10:14:13 +0200

    Formátování grafů
    Obrázek se čtyřmi grafy a obrázek se změnou mezi erou a merrou, sjednocené colorbary, správné popisy os

--- graphs.py
+++ graphs.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 def den_datum(den, rok, *p):
     prvni_den = datetime(rok, 1, 1)
     if p:
-        cilovy_datum = prvni_den + timedelta(days=den - 1)/8
+        cilovy_datum = prvni_den + timedelta(days=(den - 1) / 8)
     else:
         cilovy_datum = prvni_den + timedelta(days=den - 1)
     return cilovy_datum.strftime("%d/%m/%Y")
@@ -19,17 +19,12 @@ def datum_den(datum):
     rozdil = (dt.replace(year=rok) - prvni_den).days + 1
     return rozdil
 
-def dates(s, inp, out, *p):
-    x = len(out)
-    if p:
-        for i in range(len(inp.variables['time'][:])):
-            if (i + x) % 150 == 0:
-                out.append(den_datum(datum_den(s) + i + x, 2019))
-    else:
-        for i in range(len(inp.variables['valid_time'][:])):
-            if (i + x) % 15 == 0:
-                out.append(den_datum(datum_den(s) + i + x, 2019))
-    return out
+def dates(s, inp, out, x):
+    for i in range(len(inp.variables['valid_time'][:])):
+        if (i + x) % 15 == 0:
+            out.append(den_datum(datum_den(s) + i + x, 2019))
+    x += len(inp.variables['valid_time'][:])
+    return out, x
 
 def av(inp, out, x):
     for i in range(len(inp)):
@@ -38,121 +33,156 @@ def av(inp, out, x):
             a = np.array(inp[i][j])
             b = np.mean(a)
             out[i + x].append(b)
-        print(x + i)
     x += len(inp)
     return x, out
 
+def prumeruj_po_8(data):
+    n = len(data)
+    nove_data = []
+    for i in range(0, n, 8):
+        blok = data[i:i+8]
+        if len(blok) == 8:
+            avg = np.mean(blok, axis=0)
+            nove_data.append(avg)
+    return np.array(nove_data)
+
 def euf():
-    eu6 = Dataset('eu6.nc')
-    eu7 = Dataset('eu7.nc')
-    eu8 = Dataset('eu8.nc')
-    eu9 = Dataset('eu9.nc')
-
-    eu6_2 = eu6.variables['u']
-    eu7_2 = eu7.variables['u']
-    eu8_2 = eu8.variables['u']
-    eu9_2 = eu9.variables['u']
-
-    p = [x for x in eu6.variables['pressure_level'][:]]
-    ti = []
-    ti = dates("01/06/2019", eu6, ti)
-    ti = dates("01/07/2019", eu7, ti)
-    ti = dates("01/08/2019", eu8, ti)
-    ti = dates("01/09/2019", eu9, ti)
-
-    eu = []
-    x = 0
-
-    x, eu = av(eu6_2, eu, x)
-    x, eu = av(eu7_2, eu, x)
-    x, eu = av(eu8_2, eu, x)
-    x, eu = av(eu9_2, eu, x)
-
-    return eu, ti, p
-
-def etf():
-    et6 = Dataset('et6.nc')
-    et7 = Dataset('et7.nc')
-    et8 = Dataset('et8.nc')
-    et9 = Dataset('et9.nc')
-
-    et6_2 = et6.variables['t']
-    et7_2 = et7.variables['t']
-    et8_2 = et8.variables['t']
-    et9_2 = et9.variables['t']
-
-    et = []
-    x = 0
-
-    x, et = av(et6_2, et, x)
-    x, et = av(et7_2, et, x)
-    x, et = av(et8_2, et, x)
-    x, et = av(et9_2, et, x)
-
-    return et
-
-def muf():
-    folder = "/media/ondrej/f33d1d42-ade5-41fb-98df-bd6fefb6cf63/dokument/AV/2025 - stratosferické ohřevy/Merra"
-    files = sorted([f for f in os.listdir("/media/ondrej/f33d1d42-ade5-41fb-98df-bd6fefb6cf63/dokument/AV/2025 - stratosferické ohřevy/Merra") if f.endswith(".nc")], key=lambda x: int(x[1:].split('.')[0]))
+    folder = "./"
+    files = ["eu6.nc", "eu7.nc", "eu8.nc", "eu9.nc"]
     data = []
     time_labels = []
     x = 0
+    z = 0
+    for f in files:
+        dataset = Dataset(os.path.join(folder, f))
+        var_data = dataset.variables['u'][:]
+        pressure_levels = dataset.variables['pressure_level'][:]
+        time_labels, z = dates(f"01/06/2019", dataset, time_labels, z)
+        x, data = av(var_data, data, x)
+    print("euf done")
+    return data, time_labels, pressure_levels
 
+def etf():
+    folder = "./"
+    files = ["et6.nc", "et7.nc", "et8.nc", "et9.nc"]
+    data = []
+    x = 0
+    for f in files:
+        dataset = Dataset(os.path.join(folder, f))
+        var_data = dataset.variables['t'][:]
+        x, data = av(var_data, data, x)
+    print("etf done")
+    return data
+
+def muf():
+    folder = "./Merra"
+    files = sorted([f for f in os.listdir(folder) if f.endswith(".nc")], key=lambda x: int(x[1:].split('.')[0]))
+    data = []
+    x = 0
+    z = 0
     for f in files:
         filepath = os.path.join(folder, f)
         dataset = Dataset(filepath)
         var_data = dataset.variables['U'][:]
-        pressure_levels = dataset.variables['lev'][:]
-        time_labels = dates("01/06/2019", dataset, time_labels, True)
+        var_data = prumeruj_po_8(var_data)
         x, data = av(var_data, data, x)
-
-    return data, time_labels, pressure_levels
+    print("muf done")
+    return data
 
 def mtf():
-    folder = "/media/ondrej/f33d1d42-ade5-41fb-98df-bd6fefb6cf63/dokument/AV/2025 - stratosferické ohřevy/Merra"
-    files = sorted([f for f in os.listdir("/media/ondrej/f33d1d42-ade5-41fb-98df-bd6fefb6cf63/dokument/AV/2025 - stratosferické ohřevy/Merra") if f.endswith(".nc")], key=lambda x: int(x[1:].split('.')[0]))
+    folder = "./Merra"
+    files = sorted([f for f in os.listdir(folder) if f.endswith(".nc")], key=lambda x: int(x[1:].split('.')[0]))
     data = []
     x = 0
-
     for f in files:
         filepath = os.path.join(folder, f)
         dataset = Dataset(filepath)
         var_data = dataset.variables['T'][:]
+        var_data = prumeruj_po_8(var_data)
         x, data = av(var_data, data, x)
-
+    print("mtf done")
     return data
 
 eu, ti, p = euf()
 et = etf()
-mu, ti2, p2 = muf()
+mu = muf()
 mt = mtf()
+
+eu_arr = np.array(eu)
+mu_arr = np.array(mu)
+et_arr = np.array(et)
+mt_arr = np.array(mt)
+
+eu_diff = eu_arr - mu_arr
+et_diff = et_arr - mt_arr
 
 fig, axs = plt.subplots(2, 2, figsize=(20, 12))
 axs = axs.flatten()
 
-axs[0].contourf(list(map(list, zip(*eu))), cmap='coolwarm', levels=16)
-axs[0].set_title('Zonal wind at Southern Hemisphere from ERA5')
-axs[0].set_ylabel("Pressure level (hPa)")
-axs[0].set_xticklabels(ti)
-axs[0].set_yticklabels(p)
+def nastav_ose_x(ax, labels, total_len):
+    xticklabels = ['' for _ in range(total_len)]
+    for i in range(0, total_len, 15):
+        if i // 15 < len(labels):
+            xticklabels[i] = labels[i // 15]
+    ax.set_xticks(range(total_len))
+    ax.set_xticklabels(xticklabels, rotation=0)
+    ax.set_xlabel("Time")
 
-axs[1].contourf(list(map(list, zip(*mu))), cmap='coolwarm', levels=16)
-axs[1].set_title('Zonal wind at Southern Hemisphere from MERRA2')
-axs[1].set_ylabel("Pressure level (hPa)")
-axs[1].set_xticklabels(ti2)
-axs[1].set_yticklabels(p2)
+def nastav_ose_y(ax, labels, total_len):
+    ax.set_yticks(range(total_len))
+    ax.set_yticklabels(labels, rotation=0)
 
-axs[2].contourf(list(map(list, zip(*et))), cmap='coolwarm', levels=16)
-axs[2].set_title('Temperature at Southern Hemisphere from ERA5')
-axs[2].set_ylabel("Pressure level (hPa)")
-axs[2].set_xticklabels(ti)
-axs[2].set_yticklabels(p)
+def vykresli(ax, data, title, xlabels, ylabels, vmin=None, vmax=None, cmap='coolwarm'):
+    cont = ax.contourf(np.array(data).T, cmap=cmap, levels=16, vmin=vmin, vmax=vmax)
+    ax.set_title(title)
+    ax.set_ylabel("Pressure level (hPa)")
+    ax.set_xticks(range(len(data)))
+    ax.set_xticklabels([xlabels[i // 15] if i % 15 == 0 and i // 15 < len(xlabels) else '' for i in range(len(data))])
+    ax.set_yticks(range(len(ylabels)))
+    ax.set_yticklabels(ylabels)
+    return cont
 
-axs[3].contourf(list(map(list, zip(*mt))), cmap='coolwarm', levels=16)
-axs[3].set_title('Zonal wind at Southern Hemisphere from MERRA2')
-axs[3].set_ylabel("Pressure level (hPa)")
-axs[3].set_xticklabels(ti2)
-axs[3].set_yticklabels(p2)
+vmin_wind = min(np.min(eu), np.min(mu))
+vmax_wind = max(np.max(eu), np.max(mu))
+vlim_diff_wind = np.max(np.abs(eu_diff))
+
+vmin_temp = min(np.min(et), np.min(mt))
+vmax_temp = max(np.max(et), np.max(mt))
+vlim_diff_temp = np.max(np.abs(et_diff))
+
+fig, axs = plt.subplots(2, 2, figsize=(20, 12))
+axs = axs.flatten()
+
+tituly = [
+    "Zonal wind at SH from ERA5",
+    "Zonal wind at SH from MERRA2",
+    "Temperature at SH from ERA5",
+    "Temperature at SH from MERRA2"
+]
+data_sady = [eu, mu, et, mt]
+vmin_sady = [vmin_wind, vmin_wind, vmin_temp, vmin_temp]
+vmax_sady = [vmax_wind, vmax_wind, vmax_temp, vmax_temp]
+
+for ax, title, data, vmin_, vmax_ in zip(axs, tituly, data_sady, vmin_sady, vmax_sady):
+    cbar = vykresli(ax, data, title, ti, p, vmin=vmin_, vmax=vmax_)
+    fig.colorbar(cbar, ax=ax)
 
 plt.tight_layout()
-plt.show()
+fig.savefig("vse.png")
+plt.close(fig)
+
+fig_diff, axs_diff = plt.subplots(2, 1, figsize=(10, 12))
+tituly_diff = [
+    "ERA5 - MERRA2 Zonal wind difference",
+    "ERA5 - MERRA2 Temperature difference"
+]
+data_diff = [eu_diff, et_diff]
+vlims_diff = [vlim_diff_wind, vlim_diff_temp]
+
+for ax, title, data, vlim in zip(axs_diff, tituly_diff, data_diff, vlims_diff):
+    cbar = vykresli(ax, data, title, ti, p, vmin=-vlim, vmax=vlim, cmap='bwr')
+    fig_diff.colorbar(cbar, ax=ax)
+
+plt.tight_layout()
+fig_diff.savefig("rem.png")
+plt.close(fig_diff)
